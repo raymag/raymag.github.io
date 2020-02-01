43ead4e23de275d8d6154d2648c51b3e371a7883
raymag <prayk11@outlook.com>
2020-02-01 15:26:41 -0300

    Updates

---
+++ precache-manifest.b6971a6ede73a169c78c6a80eda2859f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55427676a6c0f57d61a46f1a3562ca76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438a3072a7b6f65d1c93",
+    "url": "/static/css/main.17173b2d.chunk.css"
+  },
+  {
+    "revision": "95db9a882b721ae4ec76",
+    "url": "/static/js/2.ff49dbd7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ff49dbd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438a3072a7b6f65d1c93",
+    "url": "/static/js/main.e6845439.chunk.js"
+  },
+  {
+    "revision": "89684ed0a9bc944cbb43",
+    "url": "/static/js/runtime-main.73e5d9c0.js"
+  },
+  {
+    "revision": "f1b0fe4fadae51b50b86ce3648cf966c",
+    "url": "/static/media/theme.f1b0fe4f.png"
+  }
+]);
