362e39ac2a81d5b3d44b1968776b1411cf939a7f
raymag <prayk11@outlook.com>
2020-02-06 12:37:43 -0300

    Updates

---
+++ precache-manifest.f4f081f8503a4d53e49263e9d5de002e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3148583d9e6a59c4ba335b6c35ec77fb",
+    "url": "https://raymag.github.io/index.html"
+  },
+  {
+    "revision": "56489a97397da73e5d69",
+    "url": "https://raymag.github.io/static/css/main.64a2c2af.chunk.css"
+  },
+  {
+    "revision": "19a22fd39913c7c94fe2",
+    "url": "https://raymag.github.io/static/js/2.1c2a1f28.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "https://raymag.github.io/static/js/2.1c2a1f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56489a97397da73e5d69",
+    "url": "https://raymag.github.io/static/js/main.8b302b78.chunk.js"
+  },
+  {
+    "revision": "a2adbcd2defdf5bcf469",
+    "url": "https://raymag.github.io/static/js/runtime-main.9db8816c.js"
+  },
+  {
+    "revision": "f1b0fe4fadae51b50b86ce3648cf966c",
+    "url": "https://raymag.github.io/static/media/theme.f1b0fe4f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2a060f979babd759f3b8cc1f6e38342.js"
+  "https://raymag.github.io/precache-manifest.f4f081f8503a4d53e49263e9d5de002e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://raymag.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
