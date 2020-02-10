104651c7f21cc315c8e735f50eb08cb636f10dbc
raymag <prayk11@outlook.com>
2020-02-10 13:10:12 -0300

    Add new BlogPosts

--- src/data/BlogPosts.js
+++ src/data/BlogPosts.js
@@ -1,13 +1,18 @@
 const BlogPosts = [
     {
-        'title': 'Building a Simple WebChat',
-        'to': 'https://dev.to/raymag/building-a-simple-webchat-2504',
-        'footer': 'Learn how to build a webchat with Python and Websockets.'
-    },
-    {
         'title': 'Build a Quiz REST API with NodeJS',
         'to': 'https://dev.to/raymag/build-a-quiz-rest-api-with-nodejs-2p64',
         'footer': 'Build a simple Quiz REST API using NodeJS and MongoDB'
+    },
+    {
+        'title': 'Deploying a NodeJS app to Heroku',
+        'to': 'https://dev.to/raymag/deploying-a-nodejs-app-to-heroku-2mho',
+        'footer': 'Step-to-Step guide to how to deploy a NodeJS app to Heroku.'
+    },
+    {
+        'title': 'Building a Simple WebChat',
+        'to': 'https://dev.to/raymag/building-a-simple-webchat-2504',
+        'footer': 'Learn how to build a webchat with Python and Websockets.'
     },
     {
         'title': 'Getting Started with Flask',
