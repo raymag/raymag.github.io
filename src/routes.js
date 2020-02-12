a3d3362a5b896370aeb69fa38d4ab2a413d7c3d6
raymag <prayk11@outlook.com>
2020-02-12 13:48:14 -0300

    Add page transitions

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import React from 'react';
 
 import Home from './pages/Home';
@@ -9,18 +10,30 @@ import e404 from './pages/e404';
 
 import NavBar from './components/NavBar';
 
+import './App.css';
+
 export default function Routes(){
     return (
         <>
         <HashRouter>
         <NavBar />
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/skills' component={Skills} />
-                <Route path='/misc' component={Misc} />
-                <Route path='/about' component={About} />
-                <Route component={e404} />
-            </Switch>
+        <Route render={({location}) => (
+            <TransitionGroup component={null}>
+            <CSSTransition 
+                key={location.pathname}
+                classNames="page"
+                timeout={400}
+            >
+                <Switch location={location}>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/skills' component={Skills} />
+                    <Route path='/misc' component={Misc} />
+                    <Route path='/about' component={About} />
+                    <Route component={e404} />
+                </Switch>
+                </CSSTransition>
+          </TransitionGroup>
+        )} />
         </HashRouter>
         </>
     )
