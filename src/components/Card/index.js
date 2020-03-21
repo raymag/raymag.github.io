848fd168cceb1e1400bc0bc765a2d7d2c186c877
raymag <prayk11@outlook.com>
2020-03-21 16:08:29 -0300

    add loading effect on Misc and Skills page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 
 export default function Card({to, title, thumbnail, body, footer, dark, simple, clean}){
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [thumbnailState, setThumbnailState] = useState('loading');
+
+    function showThumbnail(){
+        setTimeout(()=>{
+            setIsLoading(false);
+            setThumbnailState('');
+        }, 300);
+    }
+
     return (
         <a href={to} target="_blank" title={title} rel="noopener noreferrer" className={ dark ? "react-card dark" : "react-card" }>
             <div className="card-header">
-                {thumbnail ? <img src={thumbnail} alt="thumbnail"/> : ''}
+                {thumbnail && isLoading ? <img src="./assets/loading.gif" alt="loading" /> : ''}
+                {thumbnail ? <img className={thumbnailState} onLoad={showThumbnail} src={thumbnail} alt="thumbnail" /> : ''}
                 {simple ? '' : (<h3>{title}</h3>)}
             </div>
             {clean ? '' : (<div className={simple ? "card-body simple" : "card-body"}>
