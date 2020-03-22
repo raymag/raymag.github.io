a57cd58f340719fb4638378e5944d4f9128c21fd
raymag <prayk11@outlook.com>
2020-03-21 21:59:02 -0300

    add SearchBar to Repos page

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react';
 import Box from '../../components/Box';
 import Postit from '../../components/Postit';
 import PageController from '../../components/PageController';
+import SearchBar from '../../components/SearchBar';
 
 import './styles.css';
 
 export default function Repos(){
     const itemsPerPage = 15;
     const [repos, setRepos] = useState([]);
+    const [query, setQuery] = useState('');
     const [pagesNumber, setPagesNumber] = useState(1);
     const [currentPage, setCurrentPage] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
@@ -17,13 +19,27 @@ export default function Repos(){
             await fetch("https://api.github.com/users/raymag/repos?sort=updated")
                     .then(res => res.json())
                     .then(data => {
-                        setIsLoading(false);
-                        setRepos(data);
-                        setPagesNumber(Math.ceil(data.length/itemsPerPage));
+                        setIsLoading(false);                        
+                        const foundRepos = [];
+                        data.map((repo, index) => {
+                            const regex = new RegExp(query, 'ig');
+                            if(regex.test(repo["name"])){
+                                foundRepos.push(repo);
+                            }
+                            return null;
+                        });
+                        const pages = Math.ceil(foundRepos.length/itemsPerPage);
+                        setRepos(foundRepos);
+                        setCurrentPage(1);
+                        if(pages === 0){
+                            setPagesNumber(1);
+                        }else{
+                            setPagesNumber(pages);
+                        }
                     });
         }
         getRepos();
-    }, []);
+    }, [query]);
 
     function previousPage(){
         if(currentPage>1){
@@ -35,11 +51,16 @@ export default function Repos(){
             setCurrentPage(currentPage+1);
         }
     }
-
+    function search(e){
+        setQuery(e.target.value);
+    }
     return (
         <>
-
         <Box title="Repositories">
+            <SearchBar
+                placeholder={"Search Repositories"}
+                search={search}
+            />
         <PageController
             previousPage={previousPage}
             nextPage={nextPage}
