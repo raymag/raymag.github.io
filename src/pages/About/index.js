99a41bff79abdab7db315693506ecd7960580d1a
raymag <prayk11@outlook.com>
2020-03-22 19:47:45 -0300

    update bio in About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,14 +5,18 @@ import './styles.css';
 
 export default function About(){
     return (
-        <Box color="#e5e5e5" bgColor="#421567" title="Carlos Magno (raymag)" column >
-            <img src="./assets/dev.jpg" alt="dev" id="dev-pic"/>
-            <p id="bio">
-            Introvert, Geek, Web Developer, Horde Player and CS Student<br/>
-            System.out.println(<span className="highlight">"H3llo W0rld"</span>);
-            </p><br/>
-            <p>prayk11@gmail.com</p>
-            <p>prayk11@outlook.com</p>
+        <Box color="#e5e5e5" bgColor="#421567" title="Carlos Magno" column >
+            <div className="bio-container">
+                <img src="./assets/dev.jpg" alt="dev" id="dev-pic" title="Magno"/>
+                <div className="bio-text">
+                    <p id="bio">
+                    Introvert, Geek, Web Developer, Horde Player and CS Student...<br/>
+                    Also known as <span className="italic">Raymag</span>, his hobbies are watching animations, learning new stuff, reading useful books and running.
+                    </p><br/>
+                    <p>prayk11@gmail.com</p>
+                    <p>prayk11@outlook.com</p>
+                </div>
+            </div>
         </Box>
     )
 }
