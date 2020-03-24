ba02021199cd5050847486f7fcc40189d4859793
raymag <prayk11@outlook.com>
2020-03-23 23:53:45 -0300

    Add new BlogPost

--- src/data/BlogPosts.js
+++ src/data/BlogPosts.js
@@ -5,6 +5,11 @@ const BlogPosts = [
         'footer': 'Build a simple Quiz REST API using NodeJS and MongoDB'
     },
     {
+        'title': 'Build a Slideshow Component with ReactJS ',
+        'to': 'https://dev.to/raymag/build-a-slideshow-component-with-reactjs-6ec',
+        'footer': 'Step-to-Step tutorial of how to build a Slideshow component with ReactJS'
+    },
+    {
         'title': 'Deploying a NodeJS app to Heroku',
         'to': 'https://dev.to/raymag/deploying-a-nodejs-app-to-heroku-2mho',
         'footer': 'Step-to-Step guide to how to deploy a NodeJS app to Heroku.'
