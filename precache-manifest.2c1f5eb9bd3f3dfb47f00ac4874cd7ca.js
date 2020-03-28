29405d136987bc532a0ec659847fbd578a34bcdb
raymag <prayk11@outlook.com>
2020-03-28 01:22:09 -0300

    Updates

--- precache-manifest.2c1f5eb9bd3f3dfb47f00ac4874cd7ca.js
+++ precache-manifest.2c1f5eb9bd3f3dfb47f00ac4874cd7ca.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "01bd25ab2bb70302ca3ec887b975ee2b",
+    "revision": "1ad8dea171c906d4df5d3f803b2c242a",
     "url": "/index.html"
   },
   {
-    "revision": "147faf05e7ac00d0165e",
-    "url": "/static/css/main.af65b0b2.chunk.css"
+    "revision": "ab8a5be81b8d581a4b62",
+    "url": "/static/css/main.20c7da5b.chunk.css"
   },
   {
     "revision": "6f172e0a2cb3f4c663de",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bdd44165.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "147faf05e7ac00d0165e",
-    "url": "/static/js/main.e816bb03.chunk.js"
+    "revision": "ab8a5be81b8d581a4b62",
+    "url": "/static/js/main.23d6a70a.chunk.js"
   },
   {
     "revision": "89684ed0a9bc944cbb43",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc4b523a9db109ee8078eada905c995a.js"
+  "/precache-manifest.2c1f5eb9bd3f3dfb47f00ac4874cd7ca.js"
 );
 
 self.addEventListener('message', (event) => {
