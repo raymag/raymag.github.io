3815faac0d867c9f9553310af5dd8b0cfe8588f5
raymag <prayk11@outlook.com>
2020-06-19 10:43:53 -0300

    add social media links to about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Box from '../../components/Box';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faDev, faInstagram, faTwitter, faStackOverflow, faLinkedin, faReddit } from '@fortawesome/free-brands-svg-icons';
 // import Slideshow from '../../components/Slideshow';
 
 import './styles.css';
@@ -18,6 +20,83 @@ export default function About(){
                     </p><br/>
                     <p>prayk11@gmail.com</p>
                     <p>prayk11@outlook.com</p>
+
+
+            <br></br>
+                    <ul id="footer-links">
+            <li>
+                <a
+                href="https://github.com/raymag"
+                id="github-icon"
+                target="_blank"
+                title="Github"
+                alt="github"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faGithub} />
+                </a>
+            </li>
+            <li>
+                <a
+                href="https://dev.to/raymag"
+                id="dev-icon"
+                target="_blank"
+                title="Dev"
+                alt="dev"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faDev} /></a>
+            </li>
+            <li>
+                <a
+                href="https://pt.stackoverflow.com/users/83459/carlos-magno"
+                id="stackoverflow-icon"
+                target="_blank"
+                title="StackOverflow"
+                alt="StackOverflow"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faStackOverflow} /></a>    
+            </li>
+            <li>
+                <a
+                href="https://www.linkedin.com/in/carlos-magno-n-10a2a0129/"
+                id="linkedin-icon"
+                target="_blank"
+                title="Linkedin"
+                alt="linkedin"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faLinkedin} /></a>    
+            </li>
+            <li>
+                <a
+                href="https://instagram.com/blayde88"
+                id="instagram-icon"
+                target="_blank"
+                title="Instagram"
+                alt="instagram"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faInstagram} /></a>    
+            </li>
+            <li>
+                <a
+                href="https://twitter.com/C_IMagno"
+                id="twitter-icon"
+                target="_blank"
+                title="Twitter"
+                alt="twitter"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faTwitter} /></a>    
+            </li>
+            <li>
+                <a
+                href="https://www.reddit.com/user/Blayde88"
+                id="reddit-icon"
+                target="_blank"
+                title="Reddit"
+                alt="reddit"
+                rel="noopener noreferrer"
+                ><FontAwesomeIcon icon={faReddit} /></a>    
+            </li>
+
+        </ul>
                 </div>
             </div>
         </Box>
