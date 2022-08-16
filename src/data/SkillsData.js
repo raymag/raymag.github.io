edfeee67f83b196f9947df1799fb18532bbc6be3
raymag <prayk11@outlook.com>
2022-08-16 18:08:35 -0300

    feat: small update

--- src/data/SkillsData.js
+++ src/data/SkillsData.js
@@ -1,79 +1,84 @@
 const SkillsData = [
-    {
-        title: "ReactJS",
-        thumbnail: "./assets/misc/react.png",
-        category: "skill"
-    },
-    {
-        title: "NodeJS",
-        thumbnail: "./assets/skills/nodejs.png",
-        category: "skill"
-    },    
-    {
-        title: "MongoDB",
-        thumbnail: "./assets/skills/mongodb.png",
-        category: "skill"
-    },
-    {
-        title: "ExpressJS",
-        thumbnail: "./assets/skills/express.png",
-        category: "skill"
-    },
-    {
-        title: "NextJS",
-        thumbnail: "./assets/skills/nextjs.png",
-        category: "skill"
-    },
-    {
-        title: "Python",
-        thumbnail: "./assets/skills/python.png",
-        category: "skill"
-    },
-    {
-        title: "Flask",
-        thumbnail: "./assets/skills/flask.png",
-        category: "skill"
-    },
-    {
-        title: "MySQL",
-        thumbnail: "./assets/skills/mysql.png",
-        category: "skill"
-    },
-    {
-        title: "Javascript",
-        thumbnail: "./assets/skills/javascript.png",
-        category: "skill"
-    },
-    {
-        title: "Angular",
-        thumbnail: "./assets/skills/angular.svg",
-        category: "skill"
-    },
-    {
-        title: "VueJS",
-        thumbnail: "./assets/skills/vue.png",
-        category: "skill"
-    },
-    {
-        title: "PHP",
-        thumbnail: "./assets/skills/php.png",
-        category: "skill"
-    },
-    {
-        title: "HTML & CSS",
-        thumbnail: "./assets/skills/html-css.png",
-        category: "skill"
-    },
-    {
-        title: "Puppeteer",
-        thumbnail: "./assets/skills/puppeteer.png",
-        category: "skill"
-    },
-    {
-        title: "Unity",
-        thumbnail: "./assets/skills/unity.png",
-        category: "skill"
-    },
-]
+  {
+    title: "React",
+    thumbnail: "./assets/misc/react.png",
+    category: "skill",
+  },
+  {
+    title: "Node",
+    thumbnail: "./assets/skills/nodejs.png",
+    category: "skill",
+  },
+  {
+    title: "MongoDB",
+    thumbnail: "./assets/skills/mongodb.png",
+    category: "skill",
+  },
+  {
+    title: "React Native",
+    thumbnail: "./assets/misc/atom.png",
+    category: "skill",
+  },
+  {
+    title: "NextJS",
+    thumbnail: "./assets/skills/nextjs.png",
+    category: "skill",
+  },
+  {
+    title: "ExpressJS",
+    thumbnail: "./assets/skills/express.png",
+    category: "skill",
+  },
+  {
+    title: "Python",
+    thumbnail: "./assets/skills/python.png",
+    category: "skill",
+  },
+  {
+    title: "Flask",
+    thumbnail: "./assets/skills/flask.png",
+    category: "skill",
+  },
+  {
+    title: "MySQL",
+    thumbnail: "./assets/skills/mysql.png",
+    category: "skill",
+  },
+  {
+    title: "Javascript",
+    thumbnail: "./assets/skills/javascript.png",
+    category: "skill",
+  },
+  {
+    title: "Angular",
+    thumbnail: "./assets/skills/angular.svg",
+    category: "skill",
+  },
+  {
+    title: "VueJS",
+    thumbnail: "./assets/skills/vue.png",
+    category: "skill",
+  },
+  {
+    title: "PHP",
+    thumbnail: "./assets/skills/php.png",
+    category: "skill",
+  },
+  {
+    title: "HTML & CSS",
+    thumbnail: "./assets/skills/html-css.png",
+    category: "skill",
+  },
+  {
+    title: "Puppeteer",
+    thumbnail: "./assets/skills/puppeteer.png",
+    category: "skill",
+  },
+  {
+    title: "Unity",
+    thumbnail: "./assets/skills/unity.png",
+    category: "skill",
+  },
+];
 
 export default SkillsData;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,106 +1,129 @@
-import React from 'react';
-import Box from '../../components/Box';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faDev, faInstagram, faTwitter, faStackOverflow, faLinkedin, faReddit } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import Box from "../../components/Box";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faDev,
+  faInstagram,
+  faTwitter,
+  faStackOverflow,
+  faLinkedin,
+  faReddit,
+} from "@fortawesome/free-brands-svg-icons";
 // import Slideshow from '../../components/Slideshow';
 
-import './styles.css';
+import "./styles.css";
 
-export default function About(){
-    return (
-        <>
-        <Box color="#e5e5e5" bgColor="#421567" title="Carlos Magno" column >
-            <div className="bio-container">
-                <img src="./assets/dev.jpg" alt="dev" id="dev-pic" title="Magno"/>
-                <div className="bio-text">
-                    <p id="bio">
-                    Introvert, Geek, Web Developer, Horde Player and CS Student...<br/>
-                    Also known as <span className="italic">Raymag</span>, his hobbies are watching animations, learning new stuff, reading useful books and running. 
-			He also likes to draw stuff on Adobe Illustrator, but it's pretty bad at it.
-                    </p><br/>
-                    <p>prayk11@gmail.com</p>
-                    <p>prayk11@outlook.com</p>
-
+export default function About() {
+  return (
+    <>
+      <Box color="#e5e5e5" bgColor="#421567" title="Carlos Magno" column>
+        <div className="bio-container">
+          {/* <img src="./assets/dev2.jpg" alt="dev" id="dev-pic" title="Magno" /> */}
+          <div className="bio-text">
+            <p id="bio">
+              Introvert, Geek, Web Developer, Horde Player and CS Student...
+              <br />
+              Also known as <span className="italic">Raymag</span>, his hobbies
+              are watching animations, learning new stuff, reading useful books
+              and running. He also likes to draw stuff on Adobe Illustrator, but
+              it's pretty bad at it.
+            </p>
+            <br />
+            <p>prayk11@gmail.com</p>
+            <p>prayk11@outlook.com</p>
 
             <br></br>
-                    <ul id="footer-links">
-            <li>
+            <ul id="footer-links">
+              <li>
                 <a
-                href="https://github.com/raymag"
-                id="github-icon"
-                target="_blank"
-                title="Github"
-                alt="github"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faGithub} />
+                  href="https://github.com/raymag"
+                  id="github-icon"
+                  target="_blank"
+                  title="Github"
+                  alt="github"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
                 </a>
-            </li>
-            <li>
+              </li>
+              <li>
                 <a
-                href="https://dev.to/raymag"
-                id="dev-icon"
-                target="_blank"
-                title="Dev"
-                alt="dev"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faDev} /></a>
-            </li>
-            <li>
+                  href="https://dev.to/raymag"
+                  id="dev-icon"
+                  target="_blank"
+                  title="Dev"
+                  alt="dev"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faDev} />
+                </a>
+              </li>
+              <li>
                 <a
-                href="https://pt.stackoverflow.com/users/83459/carlos-magno"
-                id="stackoverflow-icon"
-                target="_blank"
-                title="StackOverflow"
-                alt="StackOverflow"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faStackOverflow} /></a>    
-            </li>
-            <li>
+                  href="https://pt.stackoverflow.com/users/83459/carlos-magno"
+                  id="stackoverflow-icon"
+                  target="_blank"
+                  title="StackOverflow"
+                  alt="StackOverflow"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faStackOverflow} />
+                </a>
+              </li>
+              <li>
                 <a
-                href="https://www.linkedin.com/in/carlos-magno-n-10a2a0129/"
-                id="linkedin-icon"
-                target="_blank"
-                title="Linkedin"
-                alt="linkedin"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faLinkedin} /></a>    
-            </li>
-            <li>
+                  href="https://www.linkedin.com/in/carlos-magno-n-10a2a0129/"
+                  id="linkedin-icon"
+                  target="_blank"
+                  title="Linkedin"
+                  alt="linkedin"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+              <li>
                 <a
-                href="https://instagram.com/blayde88"
-                id="instagram-icon"
-                target="_blank"
-                title="Instagram"
-                alt="instagram"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faInstagram} /></a>    
-            </li>
-            <li>
+                  href="https://instagram.com/blayde88"
+                  id="instagram-icon"
+                  target="_blank"
+                  title="Instagram"
+                  alt="instagram"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li>
                 <a
-                href="https://twitter.com/C_IMagno"
-                id="twitter-icon"
-                target="_blank"
-                title="Twitter"
-                alt="twitter"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faTwitter} /></a>    
-            </li>
-            <li>
+                  href="https://twitter.com/C_IMagno"
+                  id="twitter-icon"
+                  target="_blank"
+                  title="Twitter"
+                  alt="twitter"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li>
                 <a
-                href="https://www.reddit.com/user/Blayde88"
-                id="reddit-icon"
-                target="_blank"
-                title="Reddit"
-                alt="reddit"
-                rel="noopener noreferrer"
-                ><FontAwesomeIcon icon={faReddit} /></a>    
-            </li>
-
-        </ul>
-                </div>
-            </div>
-        </Box>
-        {/* <Box title="Gallery">
+                  href="https://www.reddit.com/user/Blayde88"
+                  id="reddit-icon"
+                  target="_blank"
+                  title="Reddit"
+                  alt="reddit"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faReddit} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Box>
+      {/* <Box title="Gallery">
             <Slideshow
                 interval={3000}
                 images={[
@@ -124,6 +147,6 @@ export default function About(){
                 ]}
             />
         </Box> */}
-        </>
-    )
+    </>
+  );
 }
